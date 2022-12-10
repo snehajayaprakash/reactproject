5bd99a25fea89789551dd32aaef61cc92b828c1d
unknown <snehajjayaprakash@gmail.com>
2022-12-10 15:24:12 +0530

    first commit

---
+++ hello/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+function Login(){
+    return(
+        <div className='main-login'>
+        <div className='login-contain' >
+
+        </div>
+        </div >
+    )
+}
+export default Login;
